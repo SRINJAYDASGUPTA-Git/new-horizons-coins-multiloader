cd471a75b50fd4e9b2bb97c8b0ede7b73006e0bd
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2026-05-12 16:31:00 +0530

    !feat: Added all the item definitions, not all items have textures or models, waiting for datagen.

---
+++ fabric/src/main/java/net/enderman/nhcoins/services/FabricRegistryHelper.java
@@ -0,0 +1,33 @@
+package net.enderman.nhcoins.services;
+
+import net.enderman.nhcoins.services.types.IRegistryHelper;
+import net.enderman.nhcoins.services.util.RegistryHandle;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class FabricRegistryHelper implements IRegistryHelper {
+    @Override
+    public <T extends Item> RegistryHandle<T> registerItem(String name, Function<Item.Properties, T> item) {
+        ResourceKey<Item> key = IRegistryHelper.itemKey(name);
+        Identifier id = key.identifier();
+
+        T registered = Registry.register(BuiltInRegistries.ITEM, id, item.apply(new Item.Properties().setId(key)));
+
+        return new RegistryHandle<>() {
+            @Override
+            public Identifier id() {
+                return id;
+            }
+
+            @Override
+            public T get() {
+                return registered;
+            }
+        };
+    }
+}
--- neoforge/src/main/java/net/enderman/nhcoins/NewHorizonsCoins.java
+++ neoforge/src/main/java/net/enderman/nhcoins/NewHorizonsCoins.java
@@ -1,6 +1,7 @@
 package net.enderman.nhcoins;
 
 
+import net.enderman.nhcoins.services.NeoForgeRegistryHelper;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
@@ -16,6 +17,7 @@ public class NewHorizonsCoins {
         // Use NeoForge to bootstrap the Common mod.
         Constants.LOG.info("Hello NeoForge world!");
         CommonClass.init();
+        NeoForgeRegistryHelper.register(eventBus);
 
     }
 }
